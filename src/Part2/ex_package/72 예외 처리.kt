0d80abca02b2425d328ee6726d0c119b0b409510
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-10-29 21:00:29 +0900

    <Create> Kotlin 예제 72

---
+++ src/Part2/ex_package/72 예외 처리.kt
@@ -0,0 +1,19 @@
+package Part2.ex_package
+
+import java.lang.NumberFormatException
+
+fun main(args:Array<String>){
+    try {
+        val str = "abcd"
+        val num = str.toInt()
+
+        println(num)
+    }
+    catch (e: NumberFormatException)
+    {
+        println("문자열을 숫자로 변경하지 못함")
+    }
+    finally {
+        println("프로그램 종료")
+    }
+}
