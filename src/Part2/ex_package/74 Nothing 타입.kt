74f062d79edf3019616471821160d229c459c1f1
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-10-30 10:11:06 +0900

    <Create> Kotlin 예제 74

---
+++ src/Part2/ex_package/74 Nothing 타입.kt
@@ -0,0 +1,17 @@
+package Part2.ex_package
+
+import java.lang.Exception
+
+fun throwing(): Nothing = throw Exception()
+
+fun main(args:Array<String>){
+    println("start")
+    val i : Int = throwing()
+    println(i)
+}
+
+fun validate(num: Int){
+    val result: Int =
+            if (num >= 0) num
+            else throw Exception("num이 음수입니다.")
+}
