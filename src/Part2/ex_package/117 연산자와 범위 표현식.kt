67464b43b5fbf9abe2804aa51c94c7e1c72b4864
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2021-01-06 22:59:25 +0900

    <Create> Kotlin 예제 117

---
+++ src/Part2/ex_package/117 연산자와 범위 표현식.kt
@@ -0,0 +1,14 @@
+package Part2.ex_package
+
+fun main() {
+    val oneToTen: IntRange = 1..10
+    println(5 in oneToTen)
+
+    val upperAtoZ: CharRange = 'A'..'Z'
+    if ('C' in upperAtoZ){
+        println("대문자 입니다.")
+    }
+    if ('p' in 'a'..'z'){
+        println("소문자 입니다.")
+    }
+}
