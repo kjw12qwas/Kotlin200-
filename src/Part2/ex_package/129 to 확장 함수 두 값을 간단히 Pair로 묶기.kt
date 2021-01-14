cefc1b1e0640a922706fa3092013e12bdabd93df
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2021-01-14 23:54:30 +0900

    <Create> Kotlin 예제 129

--- src/Part2/ex_package/129 to 확장 함수 두 값을 간단히 Pair로 묶기.kt
+++ src/Part2/ex_package/129 to 확장 함수 두 값을 간단히 Pair로 묶기.kt
@@ -2,9 +2,9 @@ package Part2.ex_package
 
 import kotlin.Pair
 
-infix fun <A,B> A.to(that:B): Pair128<A, B> = Pair128(this,that)
+//infix fun <A,B> A.to(that:B): Pair<String, String> = Pair128(this,that)
 
 
 fun main() {
-    val test : Pair128<Int,Double> = 10 to 3.14
+    val test : Pair<Int,Double> = 10 to 3.14
 }
